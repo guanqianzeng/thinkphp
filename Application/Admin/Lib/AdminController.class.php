ad769578496babc50e7982322208fda4c8649240
admin <admin@admin.com>
2015-06-25 10:41:03 +0800

    首次提交

---
+++ Application/Admin/Lib/AdminController.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小官
+ * Date: 2015/6/24
+ * Time: 17:38
+ */
+
+namespace Admin\Lib;
+
+use Think\Controller;
+
+class AdminController extends Controller{
+
+    public function __construct(){
+        echo 6566;
+        parent::__construct();
+    }
+
+
+}
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,32 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'MODULE_ALLOW_LIST' => array('Home','Admin'),
+
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => 3306,
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PREFIX' => 'jfsd_',
+    'DB_NAME' => 'thinkphp',
+
+
+    'DATA_CACHE_COMPRESS' => true,
+    'DATA_CACHE_PREFIX' => 'jfsd_',
+    'DATA_CACHE_TYPE' => 'File',
+    'DATA_CACHE_PATH' => '',
+
+
+    'DEFAULT_THEME' => 'Bjjfsd',
+    'TMPL_TEMPLATE_SUFFIX' => '.php',
+
+
+
+    'COOKIE_EXPIRE' => 30,
+    'COOKIE_DOMAIN' => '',
+    'COOKIE_PREFIX' => 'jfsd_',
+
+
+
+);
